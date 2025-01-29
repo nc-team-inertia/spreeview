e7d6a284862d2c90811f986595ba5181a3e380e1
Nick Midmore <nmidmore@gmail.com>
2025-01-29 12:31:59 +0000

    feat: add controller scaffolds for review, user, episode, series

---
+++ Spreeview/SpreeviewAPI/Controllers/Implementations/EpisodeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpreeviewAPI.Controllers;
+[ApiController]
+[Route("/api/[controller]")]
+public class EpisodeController : Controller
+{
+    [HttpGet]
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult GetById(int id)
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Episode episode)
+    {
+        return View();
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult Edit(int id, Episode episode)
+    {
+        return View();
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult Delete(int id)
+    {
+        return View();
+    }
+}
---
+++ Spreeview/SpreeviewAPI/Controllers/Implementations/ReviewController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpreeviewAPI.Controllers.Implementations;
+public class ReviewController : Controller
+{
+    [HttpGet]
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult GetById(int id)
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(User user)
+    {
+        return View();
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult Edit(int id, User user)
+    {
+        return View();
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult Delete(int id)
+    {
+        return View();
+    }
+}
