f2e5c472fc8bbbf4f0876649e1979cc53d84ab5c
Metajjj <joolsjannaway@gmail.com>
2025-01-31 17:15:47 +0000

    TODO test & add new themes on top of tailwind

--- Spreeview/SpreeviewFrontend/SpreeviewFrontend/tailwind.config.js
+++ Spreeview/SpreeviewFrontend/SpreeviewFrontend/tailwind.config.js
@@ -1,27 +1,43 @@
-/* @type {import('tailwindcss').Config} */
+/* @type {import('tailwindcss').Config}
+TODO isnt reference my .css?
+*/
 module.exports = {
     content: [
         "./**/*.{razor,html,cshtml}",
         "./node_modules/flowbite/**/*.js",
         "./**/CustTailwindThemes.css"
     ],
-  theme: {
-    extend: {},
-    colors: {
-        hBg: 'var(--color-hBg)',
-        hFg: 'var(--color-hFg)',
+    theme: {
+        extend: {
+            colors: {
+                hBg: 'var(--color-hBg)',
+                hFg: 'var(--color-hFg)',
 
-        bBg: 'var(--color-bBg)',
-        bFg: 'var(--color-bFg)',
-        accent: 'var(--color-accent)',
-        ImgContainer: 'var(--colour-ImgContainer)',
+                bBg: 'var(--color-bBg)',
+                bFg: 'var(--color-bFg)',
+                accent: 'var(--color-accent)',
+                ImgContainer: '#FF0000',
 
-        fBg: 'var(--color-fBg)',
-        fFg: 'var(--color-fFg)',
-      
-  },
-    plugins: [
-        require('flowbite/plugin')
-    ],
+                fBg: 'var(--color-fBg)',
+                fFg: 'var(--color-fFg)',
+            },
+        },
+        plugins: [
+            require('flowbite/plugin')
+        ],
+    }
 }
 
+/* TODO test and implement online https://playcode.io/tailwind
+
+window.tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'my-indigo': '#FF0000',
+      },
+    },
+  },
+};
+    //WORKS when adding to class text-my-indigo ( class='bg-my-indigo' )
+*/
