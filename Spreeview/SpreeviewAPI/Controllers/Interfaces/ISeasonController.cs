f976f52e3c22c094c587ee7a92f7cbb056721f59
thecodingrunner <finn.moffett@gmail.com>
2025-01-31 14:50:12 +0000

    Add Season Controller and Service layer

---
+++ Spreeview/SpreeviewAPI/Controllers/Interfaces/ISeasonController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpreeviewAPI.Controllers.Interfaces
+{
+    public interface ISeasonController
+    {
+        Task<ActionResult> GetSeason(int seriesId, int seasonNumber);
+    }
+}
