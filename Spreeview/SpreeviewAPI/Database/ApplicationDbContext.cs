abba1984eaf2abfa1e9298e574fa54f8f54944a9
Metajjj <joolsjannaway@gmail.com>
2025-01-31 11:55:33 +0000

    Isolated DBcontext related operations to its own class - relying on dependency injection

--- Spreeview/SpreeviewAPI/Database/ApplicationDbContext.cs
+++ Spreeview/SpreeviewAPI/Database/ApplicationDbContext.cs
@@ -6,6 +6,26 @@ namespace SpreeviewAPI.Database
 {
     public class ApplicationDbContext : IdentityDbContext<IdentityUser<int>, IdentityRole<int>, int> // use integer based primary key
     {
+
+
+        //Dependency inject env and user secrets to isolate and decouple
+        private IWebHostEnvironment env;
+        private IConfiguration config;
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IWebHostEnvironment env, IConfiguration config) : base(options)
+        {
+            this.env = env;
+            this.config = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (env.IsDevelopment())
+            {
+                var conStr = config.GetConnectionString("ApplicationDb") ?? throw new Exception("Missing Dev Db from UserSecrets 'ApplicationDb' - add to restore functionality");
+                options.UseSqlServer(conStr);
+            }
+        }
     }
 }
