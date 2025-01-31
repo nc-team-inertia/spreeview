ee144915ef25921f416726b66e38bea17e2c99dd
Metajjj <joolsjannaway@gmail.com>
2025-01-31 17:15:47 +0000

    TODO test & add new themes on top of tailwind

--- Spreeview/SpreeviewFrontend/SpreeviewFrontend/tailwind.config.js
+++ Spreeview/SpreeviewFrontend/SpreeviewFrontend/tailwind.config.js
@@ -5,16 +5,16 @@ module.exports = {
         // "./node_modules/flowbite/**/*.js",
         "./**/CustTailwindThemes.css"
     ],
-  theme: {
-    extend: {},
-    colors: {
-        hBg: 'var(--color-hBg)',
-        hFg: 'var(--color-hFg)',
+    theme: {
+        extend: {
+            colors: {
+                hBg: 'var(--color-hBg)',
+                hFg: 'var(--color-hFg)',
 
-        bBg: 'var(--color-bBg)',
-        bFg: 'var(--color-bFg)',
-        accent: 'var(--color-accent)',
-        ImgContainer: 'var(--colour-ImgContainer)',
+                bBg: 'var(--color-bBg)',
+                bFg: 'var(--color-bFg)',
+                accent: 'var(--color-accent)',
+                ImgContainer: '#FF0000',
 
         fBg: 'var(--color-fBg)',
         fFg: 'var(--color-fFg)',
@@ -25,3 +25,16 @@ module.exports = {
     ],
 }
 
+/* TODO test and implement online https://playcode.io/tailwind
+
+window.tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'my-indigo': '#FF0000',
+      },
+    },
+  },
+};
+    //WORKS when adding to class text-my-indigo ( class='bg-my-indigo' )
+*/
