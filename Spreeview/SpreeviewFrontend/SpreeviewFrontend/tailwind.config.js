ad073cad80d17171287e7b27a64e4c363056a712
Metajjj <joolsjannaway@gmail.com>
2025-01-31 17:07:01 +0000

    TODO test & add new themes on top of tailwind

--- Spreeview/SpreeviewFrontend/SpreeviewFrontend/tailwind.config.js
+++ Spreeview/SpreeviewFrontend/SpreeviewFrontend/tailwind.config.js
@@ -2,10 +2,23 @@
 module.exports = {
     content: [
         "./**/*.{razor,html,cshtml}",
-        // "./node_modules/flowbite/**/*.js"
+        // "./node_modules/flowbite/**/*.js",
+        "./**/CustTailwindThemes.css"
     ],
   theme: {
     extend: {},
+    colors: {
+        hBg: 'var(--color-hBg)',
+        hFg: 'var(--color-hFg)',
+
+        bBg: 'var(--color-bBg)',
+        bFg: 'var(--color-bFg)',
+        accent: 'var(--color-accent)',
+        ImgContainer: 'var(--colour-ImgContainer)',
+
+        fBg: 'var(--color-fBg)',
+        fFg: 'var(--color-fFg)',
+      
   },
     plugins: [
         // require('flowbite/plugin')
