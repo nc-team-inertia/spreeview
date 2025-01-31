7381f7fbb045f9b79351f6d7bef0000a846878f6
Metajjj <joolsjannaway@gmail.com>
2025-01-31 12:24:17 +0000

    TODO tests, adjust Tmp per user

---
+++ Spreeview/SpreeviewAPI/Database/!Tmp.cs
@@ -0,0 +1,13 @@
+﻿namespace SpreeviewAPI.Database
+{
+    public class Tmp
+    {
+        public class Dev {
+            public static string Server = "", DbName = "", User="", Pass="";
+        }
+        public class Prod
+        {
+            //TODO connection to AWS
+        }
+    }
+}
--- Spreeview/SpreeviewAPI/Database/ApplicationDbContext.cs
+++ Spreeview/SpreeviewAPI/Database/ApplicationDbContext.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.FileProviders;
 
 namespace SpreeviewAPI.Database
 {
@@ -12,20 +15,70 @@ namespace SpreeviewAPI.Database
         private IWebHostEnvironment env;
         private IConfiguration config;
 
+        public ApplicationDbContext(IWebHostEnvironment env) { this.env = env; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IWebHostEnvironment env, IConfiguration config) : base(options)
         {
             this.env = env;
             this.config = config;
+
+            Database.EnsureCreated(); //Ensure its created
         }
 
+            //TODO test when it actually runs/is called
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             if (env.IsDevelopment())
             {
-                var conStr = config.GetConnectionString("ApplicationDb") ?? throw new Exception("Missing Dev Db from UserSecrets 'ApplicationDb' - add to restore functionality");
-                options.UseSqlServer(conStr);
+                //TearDown + create for dev/test purposes?
+                //Database.EnsureDeleted(); Database.EnsureCreated();
+
+                string? conStr;
+
+                if (config != null)
+                {
+                    Console.WriteLine("+User secrets");
+                    conStr = config.GetConnectionString("ApplicationDb") ?? throw new Exception("Missing Dev Db from UserSecrets 'ApplicationDb' - add to restore functionality");
+                }
+                else
+                {
+                    Console.WriteLine("-User secrets");
+                    conStr = $"Server={Tmp.Dev.Server};Database={Tmp.Dev.DbName};User Id={Tmp.Dev.User};Password={Tmp.Dev.Pass};Trust Server Certificate=True";
+                }
+
+                options.UseSqlServer(conStr ?? throw new InvalidDataException("No connection string for dev!"));
             }
+            else if (env.IsProduction())
+            {
+                //running via .exe // compiled
+                throw new NotImplementedException("AWS connection needed!");
+            }
+            else { throw new Exception("Unexpected environment!"); }
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //Setup values to test here!
         }
     }
+
+        //Purely for migration to work when decoupling to DBcontext
+    public class E : IWebHostEnvironment
+    {
+        public string WebRootPath { get; set; } = "";
+        public IFileProvider WebRootFileProvider { get; set; } = null;
+        public string ApplicationName { get; set; } = "";
+        public IFileProvider ContentRootFileProvider { get; set; } = null;
+        public string ContentRootPath { get; set; } = "";
+        public string EnvironmentName { get; set; } = "";
+    }
+    public class ContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var c = new ApplicationDbContext(new E() { EnvironmentName = "Production" });
+
+            return c;
+        }
+    }
+
 }
