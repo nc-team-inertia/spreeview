0554b55d360608397893c81081c21c5bcc0c1f06
Nick Midmore <nmidmore@gmail.com>
2025-02-03 10:47:33 +0000

    feat: update ReviewUpdateDTO to include only review Id and content to be updated

--- Spreeview/CommonLibrary/DataClasses/ReviewModel/ReviewUpdateDTO.cs
+++ Spreeview/CommonLibrary/DataClasses/ReviewModel/ReviewUpdateDTO.cs
@@ -2,10 +2,7 @@
 {
     public class ReviewUpdateDTO : IUpdateDTO
     {
+	    public int Id { get; set; }
 		public string Contents { get; set; }
-		public int UserId { get; set; }
-		public int SeriesId { get; set; }
-		public int Season { get; set; }
-		public int Episode { get; set; }
 	}
 }
