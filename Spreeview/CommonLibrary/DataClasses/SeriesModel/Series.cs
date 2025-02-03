3ea078aaf40a68c4c2772e2b4807032169d4fbc0
thecodingrunner <finn.moffett@gmail.com>
2025-02-03 11:44:38 +0000

    Add SeriesLoader and PopularSeries components
    Added a Series loader component. Added a Backend class for making requests to the backend API. Also added a Series card component. Updated the Series DTO to include all neccessary fields. Currently having issues with displaying banner images (path is an empty string)

--- Spreeview/CommonLibrary/DataClasses/SeriesModel/Series.cs
+++ Spreeview/CommonLibrary/DataClasses/SeriesModel/Series.cs
@@ -9,14 +9,14 @@ public class Series : IEntity
 	[JsonPropertyName("id")]
     public int Id { get; set; }
     [JsonPropertyName("name")]
-    public string Name { get; set; } = "";
+    public string Name { get; set; }
     [JsonPropertyName("first_air_date")]
     public DateOnly ReleaseDate { get; set; }
     [JsonPropertyName("overview")]
-    public string Overview { get; set; } = "";
+    public string Overview { get; set; }
     [JsonPropertyName("backdrop_path")]
-	public string BannerPath { get; set; } = "";
+	public string BannerPath { get; set; }
 	[JsonPropertyName("cover_path")]
-	public string CoverPath { get; set; } = "";
+	public string CoverPath { get; set; }
     public List<Season> Seasons { get; set; } = [];
 }
---
+++ Spreeview/SpreeviewFrontend/SpreeviewFrontend/Requests/Backend.cs
@@ -0,0 +1,31 @@
+﻿using CommonLibrary.DataClasses.SeriesModel;
+using System.Text.Json;
+
+namespace SpreeviewFrontend.Requests
+{
+	public class Backend
+	{
+		public async Task<List<Series>?> GetTrendingSeries()
+		{
+			try
+			{
+				var http = new HttpClient();
+
+				var response = await http.GetFromJsonAsync<List<Series>>(
+					$"https://localhost:7119/api/Series/trending");
+
+				if (response != null)
+				{
+					Console.WriteLine(response[0].BannerPath);
+					return response;
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"An error occurred: {ex.Message}");
+				throw new Exception(ex.Message);
+			}
+			return null;
+		}
+	}
+}
