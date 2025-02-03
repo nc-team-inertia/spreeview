c65073a07db4b9174f6cef6178644bfae13c3e5b
Metajjj <joolsjannaway@gmail.com>
2025-02-03 14:55:49 +0000

    todo userPrefThemes

--- Spreeview/SpreeviewFrontend/SpreeviewFrontend/Services/UserPreferences.cs
+++ Spreeview/SpreeviewFrontend/SpreeviewFrontend/Services/UserPreferences.cs
@@ -2,5 +2,8 @@
 
 public class UserPreferences
 {
+    //TODO make static/consistent throughout the application's lifecycle
+    //TODO default to a cookie/localCache instead?
+
     public Theme Theme { get; set; } = Theme.Light;
 }
