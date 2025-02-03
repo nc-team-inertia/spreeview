83f76609e9f08908409381511f97f61232643e38
Christopher Lee <leecgh@gmail.com>
2025-02-03 11:42:46 +0000

    feat: Add tests for Episode GET endpoint

--- Spreeview/SpreeviewTests/ControllerTests/EpisodeControllerTests.cs
+++ Spreeview/SpreeviewTests/ControllerTests/EpisodeControllerTests.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
+using CommonLibrary.DataClasses.EpisodeModel;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using SpreeviewAPI.Controllers.Implementations;
+using SpreeviewAPI.MappingProfiles;
 using SpreeviewAPI.Services.Interfaces;
 
 namespace SpreeviewTests.ControllerTests;
@@ -8,19 +11,110 @@ namespace SpreeviewTests.ControllerTests;
 public class EpisodeControllerTests
 {
     Mock<IEpisodeService> _mockEpisodeService;
-    Mock<IMapper> _mockMapper;
+
+    EpisodeMappingProfile episodeMappingProfile;
+    MapperConfiguration episodeMappingConfig;
+    Mapper _mapper;
+
     EpisodeController episodeController;
 
     [SetUp]
     public void Setup()
     {
         _mockEpisodeService = new Mock<IEpisodeService>();
-        episodeController = new EpisodeController(_mockEpisodeService.Object, _mockMapper.Object);
+
+        episodeMappingProfile = new EpisodeMappingProfile();
+        episodeMappingConfig = new MapperConfiguration(config => config.AddProfile(episodeMappingProfile));
+        _mapper = new Mapper(episodeMappingConfig);
+
+        episodeController = new EpisodeController(_mockEpisodeService.Object, _mapper);
+    }
+
+    #region GetEpisodeByIds method tests
+    [Test]
+    public async Task GetEpisodeByIds_CallsServiceMethodOnce()
+    {
+        // Arrange
+        int testSeriesId = 1;
+        int testSeasonNum = 2;
+        int testEpisodeNum = 3;
+
+        Episode expectedServiceReturn = new Episode() { Id = 1, Title = "TestEpisode" };
+
+        _mockEpisodeService.Setup(mock => mock.FindEpisodeByIds(testSeriesId, testSeasonNum, testEpisodeNum))
+                           .ReturnsAsync(expectedServiceReturn);
+
+        // Act
+        await episodeController.GetEpisodeByIds(testSeriesId, testSeasonNum, testEpisodeNum);
+
+        // Assert
+        _mockEpisodeService.Verify(mock => mock.FindEpisodeByIds(testSeriesId, testSeasonNum, testEpisodeNum), Times.Once());
     }
 
     [Test]
-    public void GetEpisodeById_DoesSomething()
+    public async Task GetEpisodeByIds_OnValidRequest_ReturnsOkObjectResult()
     {
-        Assert.Pass();
+        // Arrange
+        int testSeriesId = 1;
+        int testSeasonNum = 2;
+        int testEpisodeNum = 3;
+
+        Episode expectedServiceReturn = new Episode() { Id = 1, Title = "TestEpisode" };
+
+        _mockEpisodeService.Setup(mock => mock.FindEpisodeByIds(testSeriesId, testSeasonNum, testEpisodeNum))
+                           .ReturnsAsync(expectedServiceReturn);
+
+        // Act
+        var resultObject = await episodeController.GetEpisodeByIds(testSeriesId, testSeasonNum, testEpisodeNum);
+
+        // Assert
+        Assert.That(resultObject, Is.TypeOf<OkObjectResult>());
     }
+
+    [Test]
+    public async Task GetEpisodeByIds_OnValidRequest_ReturnsMappedEpisode()
+    {
+        // Arrange
+        int testSeriesId = 1;
+        int testSeasonNum = 2;
+        int testEpisodeNum = 3;
+
+        Episode expectedServiceReturn = new Episode() { Id = 1, Title = "TestEpisode" };
+        EpisodeGetDTO expectedControllerReturn = new EpisodeGetDTO() { Id = 1, Title = "TestEpisode" };
+
+        _mockEpisodeService.Setup(mock => mock.FindEpisodeByIds(testSeriesId, testSeasonNum, testEpisodeNum))
+                           .ReturnsAsync(expectedServiceReturn);
+
+        // Act
+        var resultObject = await episodeController.GetEpisodeByIds(testSeriesId, testSeasonNum, testEpisodeNum) as OkObjectResult;
+        var resultValue = resultObject!.Value as EpisodeGetDTO;
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(resultValue!.Id, Is.EqualTo(expectedControllerReturn.Id));
+            Assert.That(resultValue.Title, Is.EqualTo(expectedControllerReturn.Title));
+        });
+    }
+
+    [Test]
+    public async Task GetEpisodeByIds_OnInvalidRequest_ReturnsNotFoundObjectResult()
+    {
+        // Arrange
+        int testSeriesId = int.MaxValue;
+        int testSeasonNum = int.MaxValue;
+        int testEpisodeNum = int.MaxValue;
+
+        Episode? expectedServiceReturn = null;
+
+        _mockEpisodeService.Setup(mock => mock.FindEpisodeByIds(testSeriesId, testSeasonNum, testEpisodeNum))
+                           .ReturnsAsync(expectedServiceReturn);
+
+        // Act
+        var resultObject = await episodeController.GetEpisodeByIds(testSeriesId, testSeasonNum, testEpisodeNum);
+
+        // Assert
+        Assert.That(resultObject, Is.TypeOf<NotFoundObjectResult>());
+    }
+    #endregion
 }
--- Spreeview/SpreeviewTests/ServiceTests/EpisodeServiceTests.cs
+++ Spreeview/SpreeviewTests/ServiceTests/EpisodeServiceTests.cs
@@ -1,6 +1,87 @@
-﻿namespace SpreeviewTests.ControllerTests;
+﻿using CommonLibrary.DataClasses.EpisodeModel;
+using Moq;
+using SpreeviewAPI.Services.Implementations;
+using SpreeviewAPI.Utilities;
+
+namespace SpreeviewTests.ControllerTests;
 
 public class EpisodeServiceTests
 {
+    Mock<IRequestManager> _mockRequestManager;
+    EpisodeService episodeService;
 
+    [SetUp]
+    public void Setup()
+    {
+        _mockRequestManager = new Mock<IRequestManager>();
+        episodeService = new EpisodeService(_mockRequestManager.Object);
+    }
+
+    #region FindEpisodeByIds method tests
+    [Test]
+    public async Task FindEpisodeByIds_CallsUtilityMethodOnce()
+    {
+        // Arrange
+        int testSeriesId = 1;
+        int testSeasonNum = 2;
+        int testEpisodeNum = 3;
+
+        string urlSuffix = $"tv/{testSeriesId}/season/{testSeasonNum}/episode/{testEpisodeNum}";
+
+        Episode expectedUtilityReturn = new Episode() { Id = 1, Title = "TestEpisode" };
+
+        _mockRequestManager.Setup(mock => mock.TmdbGetAsync<Episode>(urlSuffix))
+                           .ReturnsAsync(expectedUtilityReturn);
+
+        // Act
+        await episodeService.FindEpisodeByIds(testSeriesId, testSeasonNum, testEpisodeNum);
+
+        // Assert
+        _mockRequestManager.Verify(mock => mock.TmdbGetAsync<Episode>(urlSuffix), Times.Once());
+    }
+
+    [Test]
+    public async Task FindEpisodeByIds_OnValidRequest_ReturnsEpisodeType()
+    {
+        // Arrange
+        int testSeriesId = 1;
+        int testSeasonNum = 2;
+        int testEpisodeNum = 3;
+
+        string urlSuffix = $"tv/{testSeriesId}/season/{testSeasonNum}/episode/{testEpisodeNum}";
+
+        Episode expectedUtilityReturn = new Episode() { Id = 1, Title = "TestEpisode" };
+
+        _mockRequestManager.Setup(mock => mock.TmdbGetAsync<Episode>(urlSuffix))
+                           .ReturnsAsync(expectedUtilityReturn);
+
+        // Act
+        var result = await episodeService.FindEpisodeByIds(testSeriesId, testSeasonNum, testEpisodeNum);
+
+        // Assert
+        Assert.That(result, Is.TypeOf<Episode>());
+    }
+
+    [Test]
+    public async Task FindEpisodeByIds_OnInvalidRequest_ReturnsNull()
+    {
+        // Arrange
+        int testSeriesId = int.MaxValue;
+        int testSeasonNum = int.MaxValue;
+        int testEpisodeNum = int.MaxValue;
+
+        string urlSuffix = $"tv/{testSeriesId}/season/{testSeasonNum}/episode/{testEpisodeNum}";
+
+        Episode? expectedUtilityReturn = null;
+
+        _mockRequestManager.Setup(mock => mock.TmdbGetAsync<Episode>(urlSuffix))
+                           .ReturnsAsync(expectedUtilityReturn);
+
+        // Act
+        var result = await episodeService.FindEpisodeByIds(testSeriesId, testSeasonNum, testEpisodeNum);
+
+        // Assert
+        Assert.That(result, Is.Null);
+    }
+    #endregion
 }
