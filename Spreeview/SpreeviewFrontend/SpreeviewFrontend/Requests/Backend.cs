eebb5104938dafb61f0ea7cf5ff6270727c16a88
thecodingrunner <finn.moffett@gmail.com>
2025-02-03 15:08:30 +0000

    Update Img src for season cards in series info

--- Spreeview/SpreeviewFrontend/SpreeviewFrontend/Requests/Backend.cs
+++ Spreeview/SpreeviewFrontend/SpreeviewFrontend/Requests/Backend.cs
@@ -27,5 +27,28 @@ namespace SpreeviewFrontend.Requests
 			}
 			return null;
 		}
+
+		public async Task<SeriesGetDTO?> GetSeriesById(int id)
+		{
+			try
+			{
+				var http = new HttpClient();
+
+				var response = await http.GetFromJsonAsync<SeriesGetDTO>(
+					$"https://localhost:7119/api/Series/{id}");
+
+				if (response != null)
+				{
+					Console.WriteLine(response);
+					return response;
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"An error occurred: {ex.Message}");
+				throw new Exception(ex.Message);
+			}
+			return null;
+		}
 	}
 }
