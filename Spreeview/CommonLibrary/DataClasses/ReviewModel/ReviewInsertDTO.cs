0d252ac1dea7e185550963c0650aeb973c9f4547
Nick Midmore <nmidmore@gmail.com>
2025-02-03 10:46:03 +0000

    feat: update ReviewGetDTO with relevant props for receiving review objects from API

--- Spreeview/CommonLibrary/DataClasses/ReviewModel/ReviewInsertDTO.cs
+++ Spreeview/CommonLibrary/DataClasses/ReviewModel/ReviewInsertDTO.cs
@@ -5,7 +5,9 @@ namespace CommonLibrary.DataClasses.ReviewModel
     public class ReviewInsertDTO : IInsertDTO
     {
 		public int UserId { get; set; }
+		public int SeriesId { get; set; }
 		public int EpisodeId { get; set; }
+		[MaxLength(1000)]
 		public string Contents { get; set; }
 		[Range(1, 10)]
 		public int Rating { get; set; }
