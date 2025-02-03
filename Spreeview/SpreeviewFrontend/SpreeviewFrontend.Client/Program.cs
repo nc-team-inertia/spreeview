6400071831b89b31176a7cb02ab34e8687f34f42
Samuel M <76839403+Syntrice@users.noreply.github.com>
2025-02-03 13:34:13 +0000

    refactor(frontend): Move identity classes to server
    Move identity classes to server and restructure namespaces.

--- Spreeview/SpreeviewFrontend/SpreeviewFrontend.Client/Program.cs
+++ Spreeview/SpreeviewFrontend/SpreeviewFrontend.Client/Program.cs
@@ -1,36 +1,6 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using SpreeviewFrontend.Client.Identity;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-
-// register the cookie handler
-builder.Services.AddTransient<CookieHandler>();
-
-// set up authorization
-builder.Services.AddAuthorizationCore();
-
-// register the custom state provider
-builder.Services.AddScoped<AuthenticationStateProvider, CookieAuthenticationStateProvider>();
-
-// Cascading authentication state
-builder.Services.AddCascadingAuthenticationState();
-
-// register the account management interface (satisfy IAccountManagement dependency with our previously added AuthenticationStateProvider).
-builder.Services.AddScoped(sp => (IAccountManagement) sp.GetRequiredService<AuthenticationStateProvider>()); 
-
-// Create a named HTTP client connecting to the  backend
-string? backendUrl = builder.Configuration["BackendUrl"];
-
-if (string.IsNullOrEmpty(backendUrl))
-{
-    throw new InvalidOperationException("BackendUrl not found in configuration.");
-}
-
-builder.Services
-    .AddHttpClient("SpreeviewAPI", client => client.BaseAddress = new Uri(backendUrl))
-    .AddHttpMessageHandler<CookieHandler>();
-
 
 await builder.Build().RunAsync();
