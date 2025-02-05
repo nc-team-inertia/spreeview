7a5d439b3f1d5f09b042715822983f70b78e9d46
Samuel M <76839403+Syntrice@users.noreply.github.com>
2025-02-05 12:23:16 +0000

    feat: Add health check model classes

---
+++ Spreeview/CommonLibrary/DataClasses/HealthModel/Health.cs
@@ -0,0 +1,7 @@
+﻿namespace CommonLibrary.DataClasses.HealthModel;
+
+public class Health
+{
+    public string OverallStatus { get; set; } = string.Empty;
+    public Dictionary<string, HealthCheck> Results { get; set; } = [];
+}
---
+++ Spreeview/CommonLibrary/DataClasses/HealthModel/HealthCheck.cs
@@ -0,0 +1,7 @@
+﻿namespace CommonLibrary.DataClasses.HealthModel;
+
+public class HealthCheck
+{
+    string Status { get; set; } = string.Empty;
+    string Description { get; set; } = string.Empty;
+}
