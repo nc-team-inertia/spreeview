b7727d69cf5d0c0efe741f5f6fc54c667be0764c
Christopher Lee <leecgh@gmail.com>
2025-02-06 14:06:48 +0000

    feat: Edit Review entity and add new migration

--- Spreeview/CommonLibrary/DataClasses/ReviewModel/ReviewGetDTO.cs
+++ Spreeview/CommonLibrary/DataClasses/ReviewModel/ReviewGetDTO.cs
@@ -1,20 +1,24 @@
 ﻿using CommonLibrary.DataClasses.CommentModel;
 using System.ComponentModel.DataAnnotations;
 
-namespace CommonLibrary.DataClasses.ReviewModel
+namespace CommonLibrary.DataClasses.ReviewModel;
+
+public class ReviewGetDTO : IGetDTO
 {
-    public class ReviewGetDTO : IGetDTO
-    {
-		public int Id { get; set; }
-        public int UserId { get; set; }
-        public int SeriesId { get; set; }
-        public int EpisodeId { get; set; }
-        [MaxLength(1000)]
-        public string Contents { get; set; }
-		public DateTime DateAdded { get; set; }
-		[Range(1, 10)]
-        public int Rating { get; set; }
-		public List<Comment> Comments { get; set; } = [];
-		public int Likes { get; set; }
-	}
+	public int Id { get; set; }
+    public int UserId { get; set; }
+    public int SeriesId { get; set; }
+    public int SeasonNumber { get; set; }
+    public int EpisodeNumber { get; set; }
+    public int EpisodeId { get; set; }
+
+    [MaxLength(1000)]
+    public string Contents { get; set; } = "";
+
+	[Range(1, 10)]
+    public int Rating { get; set; }
+
+    public List<Comment> Comments { get; set; } = [];
+	public int Likes { get; set; }
+    public DateTime DateAdded { get; set; }
 }
