19145e6dd4e98e5d870fadafeead78a62b6e6407
Samuel M <76839403+Syntrice@users.noreply.github.com>
2025-02-07 08:51:43 +0000

    chore: Add documentation to ChatHub.cs

--- Spreeview/SpreeviewFrontend/SpreeviewFrontend/Hubs/ChatHub.cs
+++ Spreeview/SpreeviewFrontend/SpreeviewFrontend/Hubs/ChatHub.cs
@@ -4,6 +4,11 @@ namespace SpreeviewFrontend.Hubs;
 
 public class ChatHub : Hub
 {
+    /// <summary>
+    /// Broadcast a message to all clients in the hub. 
+    /// </summary>
+    /// <param name="user">The name of the user</param>
+    /// <param name="message">The message contents</param>
     public async Task SendMessage(string user, string message)
     {
         // Broadcast a message to all clients, using RecieveMessage, user, and message.
